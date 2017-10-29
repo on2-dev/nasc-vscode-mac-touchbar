98a58fd0f5a8884826d4b5d9ff2c59fd35befb33
Felipe <felipenmoura@gmail.com>
2017-10-29 19:23:43 -0200

    Added support for toggling panels and running commands

--- extension.js
+++ extension.js
@@ -68,6 +68,7 @@ function addCursor (direction) {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+
     const go2Def = new GoDefinitionProvider()
 //     // Use the console to output diagnostic information (console.log) and errors (console.error)
 //     // This line of code will only be executed once when your extension is activated
